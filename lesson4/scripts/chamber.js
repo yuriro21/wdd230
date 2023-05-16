78a5cee66020ed1a2bff0d6fd4b9da8be5f03190
yuriro21 <edisson.ruiz21@gmail.com>
2023-05-16 00:12:40 -0600

    .

--- lesson4/scripts/chamber.js
+++ lesson4/scripts/chamber.js
@@ -1,15 +1,26 @@
+//Button
 function toggleMenu () {
     document.getElementById('primaryNav').classList.toggle('open');
     document.getElementById('hamburgerBtn').classList.toggle('open');
-
 }
 
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
 
-//Last modified// set current year in footer
+//Dates
 const currentDate = new Date();
+let day = currentDate.getDay();
+let date = currentDate.getDate();
+let month = currentDate.getMonth();
+let year = currentDate.getFullYear();
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let currentday = days[day];
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let currentmonth = months[month];
+
 document.querySelector('#year').textContent = currentDate.getFullYear();
+document.querySelector('#currentime').textContent = `${currentday}, ${date} ${currentmonth} ${year}`;
 
 //Last modified
-document.querySelector('#lastModified').innerHTML = `Last updated: ${document.lastModified}`;
+document.querySelector('#lastModified').textContent = `Last updated: ${document.lastModified}`;
