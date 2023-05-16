bf4e9d6f77cca632cba2a02383791a5789a192be
yuriro21 <edisson.ruiz21@gmail.com>
2023-05-15 23:07:09 -0600

    .

--- lesson4/scripts/chamber.js
+++ lesson4/scripts/chamber.js
@@ -6,3 +6,10 @@ function toggleMenu () {
 
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
+
+//Last modified// set current year in footer
+const currentDate = new Date();
+document.querySelector('#year').textContent = currentDate.getFullYear();
+
+//Last modified
+document.querySelector('#lastModified').innerHTML = `Last updated: ${document.lastModified}`;
