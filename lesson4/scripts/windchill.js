e88b5fe0a36943464306acb6db9af5d6b17633ea
yuriro21 <edisson.ruiz21@gmail.com>
2023-05-29 14:29:42 -0600

    .

--- lesson4/scripts/windchill.js
+++ lesson4/scripts/windchill.js
@@ -1,7 +1,7 @@
-var temperature = document.querySelector('gradesf');
-var wind = document.querySelector('mph');
+let temperature = document.querySelector('gradesf');
+let wind = document.querySelector('mph');
 
-var windchill = 35.74 + (0.6215 * temperature) + (0.4275 * temperature - 35.75)  *  wind ^ 0.16;
-var windchillr = Math.round(windchill);
+let windchill = 35.74 + (0.6215 * temperature) + (0.4275 * temperature - 35.75)  *  wind ^ 0.16;
+let windchillr = Math.round(windchill);
 document.getElementById('windchill').innerHTML = windchill;
 
