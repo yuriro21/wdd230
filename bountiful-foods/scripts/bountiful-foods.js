46b3c7483e2b7621d678fa87e62562989251b173
yuriro21 <edisson.ruiz21@gmail.com>
2023-07-13 22:06:26 -0600

    footer created and some javascript

--- bountiful-foods/scripts/bountiful-foods.js
+++ bountiful-foods/scripts/bountiful-foods.js
@@ -6,3 +6,12 @@ function toggleMenu () {
 
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
+
+//Last modified
+document.querySelector('#lastModified').textContent = `Last updated: ${document.lastModified}`;
+
+
+//Date
+const currentDate = new Date();
+let year = currentDate.getFullYear();
+document.querySelector('#year').textContent = year;
