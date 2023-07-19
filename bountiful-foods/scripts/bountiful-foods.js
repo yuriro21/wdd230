7ffbfd2c04608b2d63a9d31f7eebdfc82fb58259
yuriro21 <edisson.ruiz21@gmail.com>
2023-07-19 15:02:41 -0600

    JavaScript added

--- bountiful-foods/scripts/bountiful-foods.js
+++ bountiful-foods/scripts/bountiful-foods.js
@@ -12,27 +12,73 @@ document.querySelector('#lastModified').textContent = `Last updated: ${document.
 
 
 //Dates
-const currentDate = new Date();
-let day = currentDate.getDay();
-let year = currentDate.getFullYear();
 
-const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let day1 = days[day + 1];
-let day2 = days[day + 2];
-let day3 = days[day + 3];
-
-document.querySelector('#day1').textContent = day1;
-document.querySelector('#day2').textContent = day2;
-document.querySelector('#day3').textContent = day3;
-document.querySelector('#year').textContent = year;
 
 // JSON Fruits
+function ordersCounter(){
+    const ordersSpace = document.getElementById('num-drinks');
+
+    if (ordersSpace) {
+        let orders = JSON.parse(localStorage.getItem('orders'));
+        ordersSpace.textContent = orders.length || 0;
+    }
+}
+ 
+ordersCounter();
 async function getFruits() {
     const response = await fetch("./data/fruits.json");
     const data = await response.json();
+    localStorage.setItem('fruitData', JSON.stringify(data));
     displayFruits(data);
 }
 
+
+
 const displayFruits = (fruits) => {
     
+    const fruit1 = document.querySelector('#fruit1');
+
+    let defaultOption = document.createElement('option');
+
+    defaultOption.setAttribute('value', '')
+    defaultOption.textContent = 'Choose a fruit';
+
+    fruit1.appendChild(defaultOption);
+
+    fruits.forEach(fruit => {
+        let option = document.createElement('option');
+        
+        option.setAttribute('value', fruit.id);
+        option.textContent = fruit.name;
+
+        fruit1.appendChild(option);
+    });
+
 }
+
+getFruits();
+
+const fruitForm = document.querySelector('#fresh-form');
+
+fruitForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const data = localStorage.getItem("fruitData");
+
+    const fruits = JSON.parse(data);
+    let select1 = document.querySelector('#fruit1');
+    let fruit1Id = Number(select1.value);
+
+    let fruit1 = fruits.find(fruit => fruit.id == fruit1Id);
+
+    alert(' fruit' + fruit1.nutritions.carbohydrates);
+
+    let order = {fruit1: fruit1, name: 'Edisson'}
+
+    let orders = JSON.parse(localStorage.getItem('orders')) || [];
+    orders.push(order);
+    localStorage.setItem('orders', JSON.stringify(orders));
+})
+
+
+
+
